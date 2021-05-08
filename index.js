6598288352551941257179393ba69999d48af342
pushkar <agnihotripush@gmail.com>
2021-05-08 19:25:17 +0530

    added fetch call

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
-// const fetch = require('fetch');
 const fetch = require('node-fetch');
 const axios= require('axios');
 
-// setInterval(() => {
-//     console.log("Hello");
-// }, 2000);
+setInterval(() => {
+ getData()   
+},2000);
 
 async function getData(){
-    const api1 = "https://jsonplaceholder.typicode.com/todos/1";
-    const api2 = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=512&date=31-03-2021";
+    const api2 = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=110001&date=31-03-2021";
 try{
-    fetch(api2)
-    .then(response => response.text())
+    fetch(api2,
+        {
+            headers:{
+                'User-Agent':'anand'
+            }
+        })
+        .then(res => res.json())
     .then(json => console.log(json))
 } catch(error){
 console.log(error);
 
 }
-
-
 }
-
-
-getData();
