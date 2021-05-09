3add9259c88183da756d656bea469c7ee5b630ed
pushkar <agnihotripush@gmail.com>
2021-05-09 22:35:06 +0530

    update

--- controller/pincodeController.js
+++ controller/pincodeController.js
@@ -8,10 +8,10 @@ const newPincode= req.body.pincode;
 const chatId=req.body.chatId;
 
  const chataar= [chatId];
-//  const getPincode= await  TelegramCode.exists({pincode:newPincode},{__v:0}); 
+ const getPincode= await  TelegramCode.exists({pincode:newPincode},{__v:0}); 
 
 //  getPincode.chatId
-//  console.log(getPincode.chatId);
+ console.log(getPincode);
 
 
 var data={
@@ -19,10 +19,10 @@ var data={
     chatId:chataar
 }
 
-const savePincode= TelegramCode(data);
-const result= await savePincode.save();
-console.log(result);
- res.send(result);
+// const savePincode= TelegramCode(data);
+// const result= await savePincode.save();
+// console.log(result);
+ res.send(getPincode);
 }catch( error){
 console.log(error);
 }
